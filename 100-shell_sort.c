1e9d50d80d1cf328c496d8384bb8027a367394e3
Jose David Caro <j.caro0207@gmail.com>
2020-02-02 18:37:06 -0500

    [Adding task 100, 101, 107, README and header]

---
+++ 100-shell_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the
+ * shell sort algorithm and the knuth sequence to create gaps (h = h * 3 + 1)
+ * ...
+ * h = (h-1)/3
+ *
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, out, in;
+	int value;
+
+	while (gap < size)
+	{
+		gap = gap * 3 + 1;
+	}
+	if (gap > size)
+		gap = (gap - 1) / 3;
+
+	while (gap > 0)
+	{
+		for (out = gap; out  < size; out++)
+		{
+			value = array[out];
+			for (in = out;
+			     in >= gap - 1 && array[in - gap] >= value;
+			     in -= gap)
+			{
+				array[in] = array[in - gap];
+			}
+			array[in] = value;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# sorting_algoritms
+
+## Learning Objectives
+
+* At least four different sorting algorithms
+* What is the Big O notation, and how to evaluate the time complexity of an algorithm
+* How to select the best sorting algorithm for a given input
+* What is a stable sorting algorithm
+
+## Files:
+
+### 1-bubble_sort.c
+* sorts an array of integers in ascending order using the Bubble sort algorithm
+### 1-insertion_sort.c
+* sorts an array of integers in ascending order using the insertion sort algorithm
+### 2-selection_sort.c
+* sorts an array of integers in ascending order using the selection sort algorithm
+### 3-quick_sort.c
+* Sorts an array of integers in ascending order using the quick sort algorithm
