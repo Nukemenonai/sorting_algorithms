241bfc18f916f60cbf24110d203473ad23822e0f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-02-04 18:29:07 +0000

    add header file

--- sort.h
+++ sort.h
@@ -31,5 +31,7 @@ void quick_sort_hoare(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge(int *array, size_t size, size_t m);
 
 #endif /* SORT_H */
