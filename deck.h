aee6ee6d43618aede0ef78f43eea47bd4d1944b9
Jose David Caro <j.caro0207@gmail.com>
2020-02-05 23:28:10 -0500

    Fix betty style in header deck.h

--- deck.h
+++ deck.h
@@ -3,12 +3,19 @@
 
 #include <stdio.h>
 
+/**
+ *enum kind_e - enumerator  of kinds of cards
+ *@SPADE: value 0
+ *@HEART: value 1
+ *@CLUB: value 2
+ *@DIAMOND: value 3
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -20,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -33,9 +40,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
